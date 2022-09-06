f990e196130f28b8a8bd558e43d1bed580c182fd
407 <407@academicos.cesde.edu.co>
2022-09-06 17:33:56 -0500

    se crea tienda hola mundo

--- tienda.js
+++ tienda.js
@@ -1,39 +1,98 @@
 //Funcion para llenar dinamicamente
 //la tienda
 
-export function llenarTienda() {
+//export function llenarTienda() {
 
 
     let productos = [
 
-        { nombre: "Batimovil a escala", precio: 400000, foto: "../img/tienda1.jpg", descripcion: "Batimovil a escala de la ultima pelicula" }
-        { nombre: "Camiseta Batman", precio: 80000, foto: "../img/tienda2.jpg", descripcion: "Camiseta original DC" }
-        { nombre: "Gorra Batman", precio: 50000, foto: "../img/tienda3.jpg", descripcion: "Gorra original DC" }
-        { nombre: "Bati termo DC", precio: 35000, foto: "../img/tienda4.jpg", descripcion: "Termo para bati chocolate" }
-        { nombre: "Batmav v Superman", precio: 300000, foto: "../img/tienda5.jpg", descripcion: "Imagen a escala de la pelicula Batman v Superman" }
-        { nombre: "Bati morral Totto", precio: 80000, foto: "../img/tienda6.jpg", descripcion: "Bolso original DC" },
-        { nombre: "Silla Gaming Batman", precio: 2000000, foto: "../img/tienda7.jpg", descripcion: "Silla comoda y original DC" }
-        { nombre: "caja de comics Batman", precio: 300000, foto: "../img/tienda8.jpg", descripcion: "comics Batman originales" }
-        { nombre: "Bati caja sorpresa", precio: 250000, foto: "../img/tienda9.jpg", descripcion: "Caja sorpresa con productos de batman" }
-        { nombre: "Bati lonchera totto", precio: 80000, foto: "../img/tienda10.jpg", descripcion: "Lonchera para llevar tus bati alimentos" }
+        { 
+            nombre: "Batimovil a escala", 
+            precio: 400000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/batiMovil.webp?alt=media&token=36e23ad8-e16a-4c89-b2ca-fb562221bfd8"], 
+            descripcion: "Batimovil a escala de la ultima pelicula",
+        },
+        
+        { 
+            nombre: "Camiseta Batman", 
+            precio: 80000, 
+            foto:["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/camisetaBatman.webp?alt=media&token=e8e22998-28c8-4d97-bc8d-9aea67c59d05"], 
+            descripcion: "Camiseta original DC", 
+        },
+
+        { 
+            nombre: "Gorra Batman", 
+            precio: 50000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/gorraBatman.webp?alt=media&token=0f404e90-d15e-4852-9b81-72df1c67938c"], 
+            descripcion: "Gorra original DC", 
+        },
+
+        { 
+            nombre: "Bati termo DC", 
+            precio: 35000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/batiTermo.webp?alt=media&token=1bfc45a1-f41c-43f7-9df2-3783b59addd5"], 
+            descripcion: "Termo para bati chocolate", 
+        },
+
+        { 
+            nombre: "Batmav v Superman", 
+            precio: 300000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/batmavSuperman.webp?alt=media&token=859d55ea-327f-4201-b46b-c976edfb182d"], 
+            descripcion: "Imagen a escala de la pelicula Batman v Superman",
+        },
+
+        { 
+            nombre: "Bati morral Totto", 
+            precio: 80000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/batiMorral.webp?alt=media&token=4e5f2cbe-94bf-4f1c-b5e7-de737e594006"], 
+            descripcion: "Bolso original DC", 
+        },
+        
+        { 
+            nombre: "Silla Gaming Batman", 
+            precio: 2000000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/sillaBatman.webp?alt=media&token=2ad0ef77-2705-41fe-9a99-5c27b9de0443"], 
+            descripcion: "Silla comoda y original DC", 
+        },
+        
+        { 
+            nombre: "caja de comics Batman", 
+            precio: 300000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/cajaBatman.webp?alt=media&token=53b38a2d-0a0c-4c3e-a896-7f430bf014e8"], 
+            descripcion: "comics Batman originales", 
+        },
+
+        { 
+            nombre: "Bati caja sorpresa", 
+            precio: 250000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/cajaSorpresa.webp?alt=media&token=2839cf5a-df6c-4e11-8f70-1a689d21159e"], 
+            descripcion: "Caja sorpresa con productos de batman", 
+        },
+
+        { 
+            nombre: "Bati lonchera totto", 
+            precio: 80000, 
+            foto: ["https://firebasestorage.googleapis.com/v0/b/fotosantojoavd.appspot.com/o/batiLonchera.webp?alt=media&token=442dd902-8bd6-48e1-9f65-c8b225f8dfac"], 
+            descripcion: "Lonchera para llevar tus bati alimentos", 
+        },
 
     ]
 
     //Rutina para recorrer el arreglo y crear las tarjetas de cada producto
 
     //1. crear una referencia a un elemento "PADRE" o "BASE" donde anclaremos los demas
-    let fila = document.createElementById("fila")
+    let fila=document.getElementById("fila")    
 
     //2. Necesito recorrer un arreglo
-    productos.foreach(function () {
+    productos.forEach(function (producto) {
 
         //3. CREAMOS LOS ELEMENTOS NECESARIOS PARA PINTAR LOS PRODUCTOS
-        let columna = document.createElement("col-4")
-        columna.classList.add("col-md-12")
+        let columna = document.createElement("div")
+        columna.classList.add("col")
 
         let tarjeta = document.createElement("div")
         tarjeta.classList.add("card")
-        tarjeta.classList.add("h-100")
+        tarjeta.classList.add("h-100","card","shadow","text-center")
 
         let imagen = document.createElement("img")
         imagen.classList.add("card-img-top")
@@ -41,10 +100,11 @@ export function llenarTienda() {
         imagen.classList.add("h-100")
         imagen.src = producto.foto
 
-        let separador = document.getElementById("hr")
+        let separador = document.createElement("h4")
         separador.classList.add("w-50")
         separador.classList.add("mx-auto")
         separador.classList.add("d-block")
+        
 
         let nombre = document.createElement("h5")
         nombre.classList.add("fw-bold")
@@ -56,6 +116,10 @@ export function llenarTienda() {
         precio.classList.add("text-center")
         precio.classList.add("text-muted")
         precio.textContent = producto.precio
+
+        let descripcion=document.createElement("P")
+        descripcion.classList.add("text-danger")
+        descripcion.textContent=producto.descripcion
 
         let botonAmpliarInfo = document.createElement("button")
         botonAmpliarInfo.setAttribute("type", "button")
@@ -72,6 +136,7 @@ export function llenarTienda() {
         tarjeta.appendChild(separador)
         tarjeta.appendChild(nombre)
         tarjeta.appendChild(precio)
+        tarjeta.appendChild(descripcion)
         tarjeta.appendChild(botonAmpliarInfo)
 
         columna.appendChild(tarjeta)
@@ -81,6 +146,5 @@ export function llenarTienda() {
 
     })
 
+//}
 
-
-}
